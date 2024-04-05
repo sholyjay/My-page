237fd63142488d747add0d33a09f4c2ae66a3ccd
Olakanmi Sola Alade <olakanmisola042@gmail.com>
2024-04-05 14:43:54 +0100

    contact.html

--- lib/wow/process.php
+++ lib/wow/process.php
@@ -2,6 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
+    $email = $_POST["subject"];
     $message = $_POST["message"];
     
     // Process the form data (e.g., send email, save to database)
@@ -9,6 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // For demonstration purposes, you can simply print the data:
     echo "Name: " . $name . "<br>";
     echo "Email: " . $email . "<br>";
+    echo "Email: " . $subject . "<br>";
     echo "Message: " . $message . "<br>";
 }
 ?>
