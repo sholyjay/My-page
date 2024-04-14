9c1412b93a41f9e848dd20a332dac6d5fd1a73a3
Olakanmi Sola Alade <olakanmisola042@gmail.com>
2024-04-14 14:10:20 +0100

    include/process.php

--- Include/process.php
+++ Include/process.php
@@ -1,6 +1,8 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
+    $name = $_POST["first_name"];
+    $name = $_POST["surname"];
+    $name = $_POST["last_name"];
     $email = $_POST["email"];
     $email = $_POST["subject"];
     $message = $_POST["message"];
@@ -8,9 +10,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Process the form data (e.g., send email, save to database)
     
     // For demonstration purposes, you can simply print the data:
-    echo "Name: " . $name . "<br>";
-    echo "Email: " . $email . "<br>";
-    echo "Email: " . $subject . "<br>";
-    echo "Message: " . $message . "<br>";
+    echo "first_name:" . $name . "<br>";
+    echo "surname:" . $name . "<br>";
+    echo "last_name:" . $name . "<br>";
+    echo "email:" . $email . "<br>";
+    echo "subject:" . $subject . "<br>";
+    echo "Message:" . $message . "<br>";
 }
 ?>
